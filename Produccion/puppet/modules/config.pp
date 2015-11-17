91f2e49ac01af86a2fff1c996b50c6b1953c8967
Rodrigo Valdes <rodrigovaldes@gmail.com>
2015-11-17 17:00:52 -0300

    Se agrega usuario cyberuser en el config.pp

---
+++ Produccion/puppet/modules/config.pp
@@ -0,0 +1,9 @@
+# First create the user, use the 'user' type.
+# See http://reductivelabs.com/trac/puppet/wiki/TypeReference#id229
+user { "cyberuser":
+   groups => 'admin',
+   comment => 'This user was created by Puppet',
+   ensure => 'present',
+   managehome => 'true',
+   shell => '/bin/bash',
+}
