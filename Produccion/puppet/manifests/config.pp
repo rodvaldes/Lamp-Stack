7d175e03cc446f6288355a69320c0f4cbb489833
root <root@cybersyn.com>
2015-11-18 08:21:33 -0500

    se agrega grupo admin para imagen DO en Produccin

--- Produccion/puppet/manifests/config.pp
+++ Produccion/puppet/manifests/config.pp
@@ -1,5 +1,12 @@
 # First create the user, use the 'user' type.
 # See http://reductivelabs.com/trac/puppet/wiki/TypeReference#id229
+
+group { 'admin':
+  ensure => 'present',
+  gid    => '1000',
+}
+
+
 user { "cyberuser":
    groups => 'admin',
    comment => 'This user was created by Puppet',
